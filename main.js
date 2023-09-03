df15d671bec9ee353dcdc76492ab9de26ae5c0dd
PANJIYAR_EDITION <133746395+PanjiyarEdition@users.noreply.github.com>
2023-09-03 17:07:37 +0530

    UPDATE

---
+++ main.js
@@ -0,0 +1,23 @@
+// main menu for mobile and small device
+let menu = document.querySelector("#menu");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+// this is for swiper
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
